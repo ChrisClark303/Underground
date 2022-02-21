e2961cb3a3eb25dfc9055eb81f79a24e09b7007e
ChrisClark303 <38655477+ChrisClark303@users.noreply.github.com>
2022-02-21 20:44:54 +0000

    Add project files.

---
+++ Underground.Engine/Class1.cs
@@ -0,0 +1,75 @@
+﻿namespace Underground.Engine
+{
+    public class Station
+    {
+        public string Name { get; }
+        public string[] Lines { get; }
+
+        public Station(string name, string[] lines)
+        {
+            Name = name;
+            Lines = lines;
+        }
+
+        public string[] CanChangeTo(string currentLine)
+        {
+            return Lines.Except(new [] { currentLine }).ToArray();
+        }
+
+        //TODO : Station should return next x stations that is available, by direction
+    }
+
+    public enum LineDirection
+    {
+        EastWest,
+        NorthSouth
+    }
+
+    public class Line
+    {
+        public string Name { get; }
+        public Station[] Stations { get; }
+        public LineDirection Direction { get; }
+
+        public Line(Station[] stations, string name, LineDirection direction)
+        {
+            Stations = stations;
+            Name = name;
+            Direction = direction;
+        }
+    }
+
+    public enum DirectionOfTravel
+    {
+        Stationary,
+        North,
+        East,
+        South,
+        West
+    }
+
+    public class Traveller
+    {
+        public Line CurrentLine { get; private set; }
+        public Station CurrentStation { get; private set; }
+        public Station? NextStation { get; private set; }
+        public Station? PreviousStation { get; private set; }
+
+        public void StartJourney(Station station, Line line)
+        {
+            CurrentLine = line;
+            CurrentStation = station;
+        }
+
+        public void ChangeLines(Line newLine)
+        {
+            var availableLines = CurrentStation.CanChangeTo(CurrentLine.Name);
+            if (!availableLines.Contains(newLine.Name))
+            {
+                throw new ArgumentException("Cannot change to that line from this station");
+            }
+
+            CurrentLine = newLine;
+        }
+    }
+}
